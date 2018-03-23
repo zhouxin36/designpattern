1f9928b748bcec2db24ff4eb077bcf1ef0493f11
zhouxin3 <zhouxin3@tuandai.com>
2018-03-23 14:24:56 +0800

    12

---
+++ dynamicproxy/src/main/java/com/zx/dynamicproxy/DynamicproxyApplication.java
@@ -0,0 +1,12 @@
+package com.zx.dynamicproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicproxyApplication.class, args);
+	}
+}
---
+++ dynamicproxy/src/test/java/com/zx/dynamicproxy/DynamicproxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zx.dynamicproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamicproxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
